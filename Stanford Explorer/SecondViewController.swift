fe5eb5b86d4988dc0596f3fe3028cdf1027e8fde
Anton Apostolatos <antonaf@stanford.edu>
2015-08-16 16:30:14 -0400

    Initial Commit

---
+++ Stanford Explorer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Stanford Explorer
+//
+//  Created by Anton Apostolatos on 8/16/15.
+//  Copyright (c) 2015 Casa, Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
